8ecd6f4ba055097d0c381e16ad8640ea0025baac
Yash05080 <yash05080@gmail.com>
2024-08-17 23:39:49 +0530

    sample database

---
+++ lib/data/mydata.dart
@@ -0,0 +1,52 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+var myData = [
+  {
+    'icon': FaIcon(FontAwesomeIcons.burger),
+    'name': 'Food',
+    'totalamount': '₹400.00',
+    'date': 'Today',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.bagShopping),
+    'name': 'Shopping',
+    'totalamount': '₹2560.00',
+    'date': 'Today',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.ticket),
+    'name': 'Entertainment',
+    'totalamount': '₹660.67',
+    'date': 'Today',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.planeDeparture),
+    'name': 'Travel',
+    'totalamount': '₹5500.00',
+    'date': 'Yesterday',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.kitMedical),
+    'name': 'Medical',
+    'totalamount': '₹360.00',
+    'date': 'Yesterday',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.phone),
+    'name': 'Recharge',
+    'totalamount': '₹778.00',
+    'date': 'Yesterday',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.house),
+    'name': 'Rent',
+    'totalamount': '₹11000.00',
+    'date': '3 Days ago',
+  },
+  {
+    'icon': FaIcon(FontAwesomeIcons.cashRegister),
+    'name': 'Others',
+    'totalamount': '₹3400.00',
+    'date': 'Today',
+  }
+];
